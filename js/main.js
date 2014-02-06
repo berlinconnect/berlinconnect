1fbb886b081f1eb37c35f646cbb53e844d9f3807
Timothy Achumba <tim@6wunderkinder.com>
2014-02-06 16:58:50 +0100

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.hero').mousemove(function(e){
     var amountMovedX = (e.pageX * 0 / 20);
-    var amountMovedY = (e.pageY * -1 / 20);
+    var amountMovedY = (e.pageY * 0 / 20);
     $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
   });
 })
