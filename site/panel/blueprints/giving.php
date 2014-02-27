76416a4d594e0ac218a87b983902d37aa226c278
Timothy Achumba <tim@6wunderkinder.com>
2014-02-27 18:31:06 +0100

    Added the blueprint for giving and tweaked some styles

---
+++ site/panel/blueprints/giving.php
@@ -0,0 +1,22 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Giving
+pages: false
+files: false
+fields:
+  title: 
+    label: Title
+    type:  text
+  subtitle: 
+    label: Sub Title
+    type:  text
+  text: 
+    label: Text
+    type:  textarea
+    size:  large
+  bankdetails: 
+    label: Text
+    type:  textarea
+    size:  large
