4a0fc914b8a0625526b2486104174aa60466e335
Timothy Achumba <tim@6wunderkinder.com>
2014-03-01 18:51:46 +0100

    Responsive fixes

--- site/templates/giving.php
+++ site/templates/giving.php
@@ -7,14 +7,12 @@
     <div class="cols">
       <div class="col giving-copy">
         <?= kirbytext($page->text()) ?>
-        <div class="left">
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_s-xclick" />
           <input type="hidden" name="hosted_button_id" value="P67SBR8J2RHYE" />
           <input type="submit" class="giving button" value="Give Now" border="0" name="submit" alt="Support us now by donating through PayPal.">
           <img alt="" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" border="0" />
           <small>All payments are made through PayPal</small>
           <div class="payment-icons"></div>
-        </div>
       </div>
       <div class="col bank-details">
         <?= kirbytext($page->bankdetails()) ?>
