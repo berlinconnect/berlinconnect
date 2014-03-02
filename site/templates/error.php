379c089a4703aba4dd7812f0d29310a844f30aca
Timothy Achumba <tim@6wunderkinder.com>
2014-03-02 02:15:29 +0100

    Added missing 404 page

---
+++ site/templates/error.php
@@ -0,0 +1,7 @@
+<?php snippet('header') ?>
+<div class="wrapper page center">
+  <div class="logo"></div>
+  <h1><?= html($page->title()) ?></h1>
+  <?= kirbytext($page->text()) ?>
+  <?php snippet('sociallinks') ?>
+</div>
