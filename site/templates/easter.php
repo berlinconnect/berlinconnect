05a121c1eeecdd556452daa40b368d7d1d3e14f7
Timothy Achumba <tim@6wunderkinder.com>
2014-03-17 20:57:54 +0100

    Added the easter page

---
+++ site/templates/easter.php
@@ -0,0 +1,17 @@
+<?php snippet('header') ?>
+<a href="/"><span class="mini-logo"></span></a>
+<h1 class="heading"><?= $page->title() ?></h1>
+<div class="event-details">
+  <h2>Easter Special &amp; Easter Sunday</h2>
+  <h2>18th April &amp; 20th April</h2>
+  <h2>Ballhaus Mitte, Ackerstr. 144</h2>
+</div>
+
+<div class="links">
+  <a href="#">The Story of Easter</a>
+  <a href="#">The Editoral</a>
+  <a href="#">Support Syria</a>
+</div>
+
+<?php snippet('sociallinks') ?>
+
