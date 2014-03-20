de9941c8bc07e7e68cfed8a0a69cf83713f046fc
Timothy Achumba <tim@6wunderkinder.com>
2014-03-20 17:51:09 +0100

    Update

--- site/templates/easter.php
+++ site/templates/easter.php
@@ -1,7 +1,13 @@
 <?php snippet('header') ?>
+<div class="graphic">
+  <div class="desert"></div>
+  <div class="tropic one"></div>
+  <div class="tropic two"></div>
+</div>
 <div class="yellow-overlay"></div>
 <div class="concrete-overlay"></div>
 <a href="/"><span class="mini-logo"></span></a>
+<div class="center">
 <h1 class="heading"><?= $page->title() ?></h1>
 <div class="event-details">
   <h2>Easter Special &amp; Easter Sunday</h2>
@@ -9,11 +15,19 @@
   <h2>Ballhaus Mitte, Ackerstr. 144</h2>
 </div>
 
-<div class="links">
-  <a href="#">The Story of Easter</a>
+<div class="about section">
+  <div class="wrapper">
+    <h1>About</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+</div>
+
+<div class="links hidden">
+  <!-- <a href="/easter/the-gospel">The Story of Easter</a> -->
+  <a href="#">Buy Tickets</a>
   <a href="#">Fashion Editoral</a>
   <a href="https://www.hillsong.com/en/syria/">Support Syria</a>
 </div>
 
-<?php snippet('sociallinks') ?>
+</div>
 
