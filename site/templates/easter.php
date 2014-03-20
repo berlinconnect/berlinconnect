b7998dd562c94f33d06d8060770516e371de8ff0
Timothy Achumba <tim@6wunderkinder.com>
2014-03-20 20:12:33 +0100

    More changes

--- site/templates/easter.php
+++ site/templates/easter.php
@@ -7,12 +7,13 @@
 <div class="yellow-overlay"></div>
 <div class="concrete-overlay"></div>
 <a href="/"><span class="mini-logo"></span></a>
-<div class="center">
+<div class="hero-image center">
 <h1 class="heading"><?= $page->title() ?></h1>
 <div class="event-details">
   <h2>Easter Special &amp; Easter Sunday</h2>
   <h2>18th April &amp; 20th April</h2>
   <h2>Ballhaus Mitte, Ackerstr. 144</h2>
+</div>
 </div>
 
 <div class="about section">
@@ -29,5 +30,5 @@
   <a href="https://www.hillsong.com/en/syria/">Support Syria</a>
 </div>
 
-</div>
+
 
