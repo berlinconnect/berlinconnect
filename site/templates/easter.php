395f69a5b282451ab044f73b3cfea38a7c509145
Timothy Achumba <tim@6wunderkinder.com>
2014-03-21 00:19:13 +0100

    Updated copy and removed unused files

--- site/templates/easter.php
+++ site/templates/easter.php
@@ -2,53 +2,39 @@
 <div class="hero-image center">
   <a href="/"><span class="mini-logo"></span></a>
   <h1 class="heading"><?= $page->title() ?></h1>
-  <div class="event-details hidden">
-    <h2>Easter Special &amp; Easter Sunday</h2>
-    <h2>18th April &amp; 20th April</h2>
-    <h2>Ballhaus Mitte, Ackerstr. 144</h2>
-  </div>
   <?php snippet('sociallinks') ?>
 </div>
 <section class="about">
   <div class="wrapper center">
-    <h1>The Story</h1>
-    <p>A story which is as inspiring as it is controversial. A story of hope and passion; of death, betrayal and sacrifice.
-    We are passionate about telling the story of Easter in the heart of our city, Berlin.</p>
-    <p>This Easter will be celebrated with a Easter Special on Good Friday hosted by Alive, an Easter Sunday and a limited clothing collection by Alive.</p>
-    <p>Easter is right at the centre of Christianity. It is the biggest date in the Christian Calendar. </p>
+    <h1><?= $page->storytitle() ?></h1>
+    <?= kirbytext($page->storycopy()) ?>
   </div>
 </section>
-<section class="fashion hidden">
+<!-- <section class="fashion hidden">
   <div class="wrapper center">
     <h1>The Fashion</h1>
     <p>Alive created a limited edition fashion collection to celebrate Easter. All proceeds will go towards relief on the refugee crisis in Syria. At Good Friday Alive will host the Easter Special Catwalk, showcasing this limited collection.
     </p>
   </div>
-</section>
+</section> -->
 
 <section class="syria">
   <div class="wrapper center">
-    <h1>The Cause</h1>
-    <p>This Easter we want to be part of the solution for one of the largest humanitarian crises that the world has felt in decades, namely the refugee crisis coming out of Syria.</p>
-    <p>You can get involved by donating individually to UNICEF or by purchasing limited collection items with proceeds going towards this cause. Our funds will help UNICEF to provide children with clean water, food, medical care, education and warm clothing.</p>
-    <a class="button">Support Syria</a>
+    <h1><?= $page->causetitle() ?></h1>
+    <?= kirbytext($page->causecopy()) ?>
   </div>
 </section>
 
 <section class="event">
   <div class="wrapper center">
-    <h1>The Event</h1>
-    <h2>Easter Special</h2>
-    <p>
-      Catwalk &amp; Live Music.<br>All proceeds go towards relief in Syria during this crisis.
-    </p>
+    <h1><?= $page->eventtitle() ?></h1>
+    <h2><?= $page->eventsubtitle() ?></h2>
+    <?= kirbytext($page->eventcopy()) ?>
     <div class="event-details">
-      <h2>18th April</h2>
-      <h2>19:00 &amp; 20:30</h2>
-      <h2>Ballhaus Mitte, Ackerstr. 144</h2>
+      <?= kirbytext($page->eventdetails()) ?>
     </div>
-    <a class="button">Buy Tickets</a>
-    <small>Tickets go on sale on March 30th</small>
+    <a class="button"><?= $page->ticketbutton() ?></a>
+    <small><?= $page->buttonsubcopy() ?></small>
   </div>
 </section>
 
