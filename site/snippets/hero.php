2ff0bc1575969dec453be150687ee0486f23c4b5
Timothy Achumba <tim@6wunderkinder.com>
2014-07-03 08:59:22 +0200

    Synced up content with live site and changed the way hero images are used

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -1,4 +1,4 @@
-<div class="hero" id="hero">
+<div class="hero" id="hero"style="background-image: url(<?php echo $page->images()->first()->url(); ?>)">
   <div class="wrapper center">
     <div class="logo"></div>
     <h1><?= $page->herotitle() ?></h1>
