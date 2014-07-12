88ced8cce7d32207eb85bcbc124ffe58c72b972e
Timothy Achumba <tim@6wunderkinder.com>
2014-07-12 07:53:54 +0200

    Fixed the form on the giving page
    It didn’t have a closing </form> tag which caused the signup form for
    dp in the park to act like a giving button. :O

--- site/templates/giving.php
+++ site/templates/giving.php
@@ -8,10 +8,11 @@
       <div class="col giving-copy">
         <?= kirbytext($page->text()) ?>
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="P67SBR8J2RHYE" />
-          <button data-track="<?= $page->title() ?>:Give Now:Online Giving Section"  type="submit" class="giving button" border="0" name="submit" alt="Support us now by donating through PayPal.">Giving</button>
-          <img alt="" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" border="0" />
-          <div class="payment-icons"></div>
+            <input type="hidden" name="hosted_button_id" value="P67SBR8J2RHYE" />
+            <button data-track="<?= $page->title() ?>:Give Now:Online Giving Section"  type="submit" class="giving button" border="0" name="submit" alt="Support us now by donating through PayPal.">Giving</button>
+            <img alt="" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" border="0" />
+            <div class="payment-icons"></div>
+          </form>
       </div>
       <div class="col bank-details">
         <?= kirbytext($page->bankdetails()) ?>
