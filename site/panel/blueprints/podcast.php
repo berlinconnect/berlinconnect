36e411620a88350eb06e9a34aa4ee7d794dcddd0
Timothy Achumba <tim@6wunderkinder.com>
2014-08-10 07:33:17 +0200

    Added the rest of the sections

---
+++ site/panel/blueprints/podcast.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Podcast
+pages: true
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  link:
+    label: Soundcloud Link
+    type:  text
+  speaker:
+    label: Speaker
+    type:  text
