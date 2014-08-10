e5b9b27017cae93cbcb659ee5eb30305bb0e0b49
Timothy Achumba <tim@6wunderkinder.com>
2014-08-10 07:39:59 +0200

    Fixed bugs and missing content

--- site/templates/transcript.php
+++ site/templates/transcript.php
@@ -2,9 +2,12 @@
 <div class="dark">
 <?php snippet('menu') ?>
 </div>
+<?php snippet('sidebar') ?>
 <div class="content">
   <div class="wrapper page transcript">
     <h1 class="center"><?= $page->title() ?></h1>
     <?= kirbytext($page->text()) ?>
   </div>
 </div>
+<?php snippet('lightbox') ?>
+<?php snippet('footer') ?>
