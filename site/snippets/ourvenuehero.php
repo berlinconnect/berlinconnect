5b4d67205de3ba73e6e785f2178b83d89871a7c0
Timothy Achumba <tim@6wunderkinder.com>
2014-09-02 10:53:52 +0200

    more fixes to the name change

--- site/snippets/ourvenuehero.php
+++ site/snippets/ourvenuehero.php
@@ -1,9 +1,7 @@
-<div class="hero" id="newvenuehero">
+<div class="hero" id="ourvenuehero">
   <div class="wrapper center">
     <!-- <div class="logo"></div> -->
-    <h1 class="one">New Season</h1>
-    <h1 class="brush two">New Location</h1>
-    <h1 class="three">New Venue</h1>
+    <h1 class="brush three">Our Venue</h1>
     <span class="entypo arrow"></span>
   </div>
 </div>
