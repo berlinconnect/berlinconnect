2898f4fab3e93c56239fa29cb3ad50c4c1373ad2
Timothy Achumba <tim@6wunderkinder.com>
2014-09-09 10:07:15 +0200

    Added the intro section

--- site/templates/heartforthehouse.php
+++ site/templates/heartforthehouse.php
@@ -3,7 +3,12 @@
 <?php snippet('sidebar') ?>
 <div class="content">
   <?php snippet('heartforthehousehero') ?>
-
+  <div class="intro">
+    <div class="wrapper">
+      <h1 class="center"><?= $page->introheading() ?></h1>
+      <div class="bodytext"><?= kirbytext($page->introcopy()) ?>
+    </div>
+  </div>
 </div>
 <?php snippet('lightbox') ?>
 <?php snippet('footer') ?>
