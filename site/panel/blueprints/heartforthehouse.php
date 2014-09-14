dcef9db8d6a73ff356f0e58492d7a4f81ba32bc1
Timothy Achumba <tim@6wunderkinder.com>
2014-09-14 02:23:23 +0200

    More improvements

---
+++ site/panel/blueprints/heartforthehouse.php
@@ -0,0 +1,80 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Our Venue
+pages: false
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  introtheading:
+    label: Intro Title
+    type:  text
+  introcopy:
+    label: Intro Copy
+    type:  textarea
+    size:  large
+    buttons:
+      - h3
+      - bold
+      - italic
+      - email
+      - link
+      - vimeo
+  danielstory:
+    label: Daniel's Story
+    type:  textarea
+    size:  medium
+  mattstory:
+    label: Matt's Story
+    type:  textarea
+    size:  medium
+  fraukeconststory:
+    label: Frauke's and Constantines' Story
+    type:  textarea
+    size:  medium
+  dennisstory:
+    label: Dennis's Story
+    type:  textarea
+    size:  medium
+  chistory:
+    label: Chi's Story
+    type:  textarea
+    size:  medium
+  annestory:
+    label: Anne's Story
+    type:  textarea
+    size:  medium
+  threewaysheading:
+    label: Title
+    type:  text
+  faithfulgiver
+    label: Title
+    type:  text
+  faithfulgivertitle
+    label: Title
+    type:  text
+  faithfulgivercopy
+    label: Faithful Giver
+    type:  textarea
+    size:  small
+  visiongiver
+    label: Title
+    type:  text
+  visiongivertitle
+    label: Title
+    type:  text
+  visiongivercopy
+    label: Faithful Giver
+    type:  textarea
+    size:  small
+  kingdomgiver
+    label: Title
+    type:  text
+  kingdomgivertitle
+    label: Title
+    type:  text
+  kingdomgivercopy
+    label: Faithful Giver
+    type:  textarea
+    size:  small
