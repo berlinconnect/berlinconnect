2c5f89ea77ea3f985bc501ccf4aca1b42df8eb75
Timothy Achumba <tim@6wunderkinder.com>
2014-10-19 17:05:42 +0200

    More people

---
+++ site/accounts/bccomms.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: bccomms
+firstname: Berlin
+lastname: Connect
+email: bccommuncationsteam@gmail.com
+password: >
+  $2a$10$6zeaiwVunHqczau4tKMbZerxEEJ7Z8juxcNIcKpOnLlVctwoVaTAS
+language: en
+role: editor
+firstName: Berlin
+lastName: Connect
---
+++ site/accounts/pasci.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: pasci
+firstName: Pascal
+lastName: Hausberger
+email: hausberger.pascal@gmail.com
+password: >
+  $2a$10$uoapujNreUFLcr6MJ0R5buoxIHgdxBgPHKund9Wjpf8805CLDP0hu
+language: en
+role: admin
