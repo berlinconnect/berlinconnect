be74b11259736490b94644c74359dbbc7808e0af
Timothy Achumba <tim@6wunderkinder.com>
2015-01-28 23:36:43 +0100

    Fixes for kirby 2

--- site/templates/home.php
+++ site/templates/home.php
@@ -22,7 +22,7 @@
     </div>
     <div class="slates">
       <?php foreach($pages->find('sections')->children()->filterBy('umbrella', 'Sundays', ',') as $section): ?>
-        <div id="<?= $section->id() ?>" class="slate <?php if (strlen($section->pagelink()) < 1): ?>dimmed<?php endif ?> <?= $section->slatesize() ?>">
+        <div id="<?= $section->uniqueid() ?>" class="slate <?php if (strlen($section->pagelink()) < 1): ?>dimmed<?php endif ?> <?= $section->slatesize() ?>">
           <?php if (strlen($section->pagelink()) != 0): ?>
             <a class="pageLink" href="<?= $section->pagelink() ?>"></a>
           <?php endif ?>
@@ -46,7 +46,7 @@
     </div>
     <div class="slates">
       <?php foreach($pages->find('sections')->children()->filterBy('umbrella','Community') as $section): ?>
-        <div id="<?= $section->id() ?>" class="slate dimmed <?= $section->slatesize() ?>">
+        <div id="<?= $section->uniqueid() ?>" class="slate dimmed <?= $section->slatesize() ?>">
           <?php if (strlen($section->pagelink()) != 0): ?>
             <a class="pageLink" href="<?= $section->pagelink() ?>"></a>
           <?php endif ?>
@@ -73,7 +73,7 @@
     </div>
     <div class="slates">
       <?php foreach($pages->find('sections')->children()->filterBy('umbrella','Social') as $section): ?>
-        <div id="<?= $section->id() ?>" class="slate dimmed <?= $section->slatesize() ?>">
+        <div id="<?= $section->uniqueid() ?>" class="slate dimmed <?= $section->slatesize() ?>">
           <?php if (strlen($section->pagelink()) != 0): ?>
             <a class="pageLink" href="<?= $section->pagelink() ?>"></a>
           <?php endif ?>
