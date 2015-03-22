f704a7370594d86bc45d69ace7cbdadbaa0c2dc1
Timothy Achumba <tim@6wunderkinder.com>
2015-03-22 23:12:43 +0100

    Added the missing blueprint for the site section

--- site/accounts/timothy.php
+++ site/accounts/timothy.php
@@ -3,7 +3,7 @@
 username: timothy
 email: timothy@achumba.com
 password: >
-  $2a$10$ZzBEBsdbg5z0YcmO7WtmLufunA2wC.5jZ6gxkJT9g.cOKl7jq64vK
+  $2a$10$Bdt/9HfRoOxQj9J2teCgH.MmB5UX1C29chz99dBlDks7uLK9vwmpa
 language: en
 role: admin
-token: 234964c4eca23f2fcc4a0bb5e75fc526fd28e62d
+token: 47f4d2ddf7c7a63f5f5fced7c74434e34aec7b7a
---
+++ site/blueprints/site.php
@@ -0,0 +1,36 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Site
+pages: false
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  author:
+    label: Author
+    type:  text
+  description:
+    label: Description
+    type:  textarea
+  copyright:
+    label: Copyright
+    type:  text
+  home:
+    label: Home
+    type:  text
+  sundays:
+    label: Sundays
+    type:  text
+  community:
+    label: Community
+    type:  text
+  socialenterprise:
+    label: Social Enterprise
+    type:  text
+  more:
+    label: More
+    type:  text
+  notice:
+    label: Notice
+    type:  textarea
