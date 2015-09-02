9362ba95e8e2f8e14e5f7592681bfe41bc4de31b
Timothy Achumba <tim@6wunderkinder.com>
2015-09-02 10:10:18 +0200

    Built the new h4h page

--- site/accounts/timothy.php
+++ site/accounts/timothy.php
@@ -6,4 +6,11 @@ password: >
   $2a$10$Bdt/9HfRoOxQj9J2teCgH.MmB5UX1C29chz99dBlDks7uLK9vwmpa
 language: en
 role: admin
-token: 47f4d2ddf7c7a63f5f5fced7c74434e34aec7b7a
+token: bd80e1c383a1ae04b8688a1c2c0b9a983c62ed0c
+history:
+  - >
+    enlarge/toolbox/podcasts/harvest-hands-healing-hands-helping-hands
+  - enlarge/toolbox/podcasts/live-generously
+  - sections/coming-up
+  - null
+  - enlarge/toolbox
--- site/templates/heartforthehouse.php
+++ site/templates/heartforthehouse.php
@@ -5,45 +5,58 @@
   <?php snippet('heartforthehousehero') ?>
   <div class="intro">
     <div class="wrapper">
-      <div class="heading-container">
+      <div class="heading-container center">
         <h1 class="center"><?= $page->introheading() ?></h1>
+        <?= $page->introcopy()->kirbytext() ?>
+        <h3>Mark & Joyce</h3>
+        <div class="mjphoto">
+
+        </div>
       </div>
     </div>
   </div>
-  <div class="threeways">
+</div>
+<div class="event">
+  <div class="content">
+    <h4 class="center">JOIN US TO HEAR MORE ABOUT HEART FOR THE HOUSE<br>30th SEPTEMBER 2015 19:00 HAUS UNGARN<br>WITH OUR SPECIAL GUEST: ANDREW DENTON FROM SYDNEY AUSTRALIA</h4>
+    <p class="center">
+      Andrew is a Kingdom Builder and Business leader with a powerful story of how Heart for the House has impacted his personal and business life with kingdom purpose.
+    </p>
+  </div>
+</div>
+<div class="threeways">
+  <div class="cols">
+    <div class="col col-33 center faithfulgiver">
+      <a id="faithfulgiver" class="thelightbox"></a>
+      <h2 class="center">Faithful Givers</h2>
+      <p><?= $page->faithfulgiver() ?></p>
+    </div>
+    <div class="col col-33 center visiongiver">
+    <a id="visiongiver" class="thelightbox"></a>
+      <h2 class="center">Vision Givers</h2>
+      <p><?= $page->visiongiver() ?></p>
+    </div>
+    <div class="col col-33 center kingdomgiver">
+      <a id="kingdomgiver" class="thelightbox"></a>
+      <h2 class="center">Kingdom Givers</h2>
+      <p><?= $page->kingdomgiver() ?></p>
+    </div>
+  </div>
+</div>
+<div class="content">
+
+  <div class="verse">
     <div class="wrapper">
-      <div class="heading-container">
-        <h1 class="center"><?= $page->threewaysheading() ?></h1>
-      </div>
-      <div class="cols">
-        <div class="col col-33 center">
-          <h2 class="center">Faithful Givers</h2>
-          <p><?= $page->faithfulgiver() ?></p>
-          <a id="faithfulgiver" class="button dark small thelightbox"><?= $site->more() ?></a>
-        </div>
-        <div class="col col-33 center">
-          <h2 class="center">Vision Givers</h2>
-          <p><?= $page->visiongiver() ?></p>
-          <a id="visiongiver" class="button dark small thelightbox"><?= $site->more() ?></a>
-        </div>
-        <div class="col col-33 center">
-          <h2 class="center">Kingdom Givers</h2>
-          <p><?= $page->kingdomgiver() ?></p>
-          <a id="kingdomgiver" class="button dark small thelightbox"><?= $site->more() ?></a>
-        </div>
-      </div>
-      <div class="center">
-        <a href="mailto:giving@berlinclc.de?Subject=I%20would%20like%20to%20find%20out%20more%20about%20Heart%20for%20the%20House" target="_top" class="button dark big"><?= $page->threewaysbutton() ?></a>
+      <div class="heading-container center">
+        <h1 class="center">1 Chronicles 29 MSG</h1>
+        <p>
+          Dear Lord, who am I, and who are these my people, that we should presume to be giving something to you? Everything comes from you; all we’re doing is giving back what we’ve been given from your generous hand..... so I have given from the heart, honestly and happily...
+        </p>
+        <a class="button">Give Online</a>
       </div>
     </div>
   </div>
-  <div class="thanks">
-    <div class="wrapper">
-      <div class="heading-container">
-        <h1 class="center"><?= $page->thankyouheading() ?></h1>
-      </div>
-    </div>
-  </div>
+
 </div>
 <div class="lightbox hidden faithfulgiver">
   <div class="lightbox-overlay"></div>
