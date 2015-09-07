3f67b7b831efb7c988c48e75bd90617d1b6e9f3b
Timothy Achumba <tim@6wunderkinder.com>
2015-09-07 08:31:18 +0200

    Final changes

--- site/templates/heartforthehouse.php
+++ site/templates/heartforthehouse.php
@@ -55,7 +55,7 @@
       <div class="sectioncontent">
         <div class="md-table">
           <h2 class="center">Kingdom Givers</h2>
-        </div>
+x        </div>
       </div>
     </div>
   </div>
@@ -71,7 +71,7 @@
         </p>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_s-xclick" />
           <input type="hidden" name="hosted_button_id" value="P67SBR8J2RHYE" />
-          <button data-track="<?= $page->title() ?>:Give Now:Online Giving Section"  type="submit" class="giving button" border="0" name="submit" alt="Support us now by donating through PayPal.">Onlien Giving</button>
+          <button data-track="<?= $page->title() ?>:Give Now:Online Giving Section"  type="submit" class="giving button" border="0" name="submit" alt="Support us now by donating through PayPal.">Online Giving</button>
           <img alt="" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" border="0" />
           <div class="payment-icons"></div>
         </form>
