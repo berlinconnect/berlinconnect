e61eed8ea56553332c22610394ea030b25b4f6f0
Timothy Achumba <tim@6wunderkinder.com>
2015-09-08 08:31:50 +0200

    Fixes to positioning

--- site/templates/heartforthehouse.php
+++ site/templates/heartforthehouse.php
@@ -32,35 +32,35 @@
     </div>
   </div>
 </div>
-<div class="threeways">
-  <div class="cols">
-    <div class="col col-33 center faithfulgiver">
-      <a id="faithfulgiver" class="thelightbox"></a>
-      <div class="sectioncontent">
-        <div class="md-table">
-          <h2 class="center">Faithful Givers</h2>
+<div class="content">
+  <div class="threeways">
+    <div class="cols">
+      <div class="col col-33 center faithfulgiver">
+        <a id="faithfulgiver" class="thelightbox"></a>
+        <div class="sectioncontent">
+          <div class="md-table">
+            <h2 class="center">Faithful Givers</h2>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col col-33 center visiongiver">
-      <a id="visiongiver" class="thelightbox"></a>
-      <div class="sectioncontent">
-        <div class="md-table">
-          <h2 class="center">Vision Givers</h2>
+      <div class="col col-33 center visiongiver">
+        <a id="visiongiver" class="thelightbox"></a>
+        <div class="sectioncontent">
+          <div class="md-table">
+            <h2 class="center">Vision Givers</h2>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col col-33 center kingdomgiver">
-      <a id="kingdomgiver" class="thelightbox"></a>
-      <div class="sectioncontent">
-        <div class="md-table">
-          <h2 class="center">Kingdom Givers</h2>
-x        </div>
+      <div class="col col-33 center kingdomgiver">
+        <a id="kingdomgiver" class="thelightbox"></a>
+        <div class="sectioncontent">
+          <div class="md-table">
+            <h2 class="center">Kingdom Givers</h2>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<div class="content">
 
   <div class="verse">
     <div class="wrapper">
